dc5208bf1e478664c248de0381a946ab195bce11
gabriel963y <gabriel963y@gmail.com>
2026-06-09 11:43:14 -0300

    update vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    preview: {
+        allowedHosts: ['tienda-online-production-27f8.up.railway.app'],
+    },
 });
